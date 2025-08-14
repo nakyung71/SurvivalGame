97ebcbdb5b64593008cef98e092124910c901c8a
ChoTamong <jogp0110@naver.com>
2025-08-14 15:43:11 +0900

    [feat] 뮤직존, 오디오 온오프 기능 추가

--- Assets/Scripts/Music/AudioManager.cs
+++ Assets/Scripts/Music/AudioManager.cs
@@ -6,7 +6,7 @@ public class AudioManager : MonoBehaviour
 
     private AudioSource bgmSource;
 
-    private void start()
+    private void awakw()
     {
         // 싱글턴 처리
         if (Instance == null)
@@ -28,32 +28,32 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    //// 음악 켜기/끄기 토글
-    //public void ToggleBGM()
-    //{
-    //    if (bgmSource == null) return;
+    // 음악 켜기/끄기 토글
+    public void ToggleBGM()
+    {
+        if (bgmSource == null) return;
 
-    //    if (bgmSource.isPlaying)
-    //    {
-    //        bgmSource.Pause(); // Stop()이 아니라 Pause() 쓰면 다시 켰을 때 이어서 재생
-    //    }
-    //    else
-    //    {
-    //        bgmSource.Play();
-    //    }
-    //}
+        if (bgmSource.isPlaying)
+        {
+            bgmSource.Pause(); // Stop()이 아니라 Pause() 쓰면 다시 켰을 때 이어서 재생
+        }
+        else
+        {
+            bgmSource.Play();
+        }
+    }
 
-    //// 음악 강제로 끄기
-    //public void StopBGM()
-    //{
-    //    if (bgmSource != null)
-    //        bgmSource.Stop();
-    //}
+    // 음악 강제로 끄기
+    public void StopBGM()
+    {
+        if (bgmSource != null)
+            bgmSource.Stop();
+    }
 
-    //// 음악 강제로 켜기
-    //public void PlayBGM()
-    //{
-    //    if (bgmSource != null && !bgmSource.isPlaying)
-    //        bgmSource.Play();
-    //}
+    // 음악 강제로 켜기
+    public void PlayBGM()
+    {
+        if (bgmSource != null && !bgmSource.isPlaying)
+            bgmSource.Play();
+    }
 }
