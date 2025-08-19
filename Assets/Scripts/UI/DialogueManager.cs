b19f1d0fab64bd4936a56d2eb8e17dce87fc09c3
nakyung71 <nakyung71@naver.com>
2025-08-19 11:04:53 +0900

    [feat] 대화 선택지 기능 추가

--- Assets/Scripts/UI/DialogueManager.cs
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public interface ITalkable
@@ -16,8 +17,11 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] GameObject dialogueUI;
     [SerializeField] TextMeshProUGUI speakerNameText;
     [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
     bool moveToNextLine;
-
+    TextMeshProUGUI button1Text;
+    TextMeshProUGUI button2Text;
     
 
     private void Awake()
@@ -27,7 +31,9 @@ public class DialogueManager : MonoBehaviour
 
     private void Start()
     {
-        
+        button1Text = button1.GetComponentInChildren<TextMeshProUGUI>(true);
+        button2Text = button2.GetComponentInChildren<TextMeshProUGUI>(true);
+
     }
 
     private void Update()
@@ -45,24 +51,46 @@ public class DialogueManager : MonoBehaviour
     IEnumerator Talk(DialogueData data)
     {
         dialogueUI.SetActive(true);
+        button1.gameObject.SetActive(false);
+        button2.gameObject.SetActive(false);
         int index = 0;
         speakerNameText.SetText(data.TalkerName);
         moveToNextLine = true;
 
         while(index<data.dialogueLines.Length)
         {
+            
             yield return new WaitUntil(() => moveToNextLine);
 
             moveToNextLine = false;
             dialogueText.SetText(data.dialogueLines[index]);
+            if (data.choiceExist && index == data.dialogueLines.Length - 1)
+            {
+                Debug.Log(index);
+                ShowButtons(data);
+            }
             index++;
         }
-        yield return new WaitUntil(() => moveToNextLine);
-        
-        dialogueUI.SetActive(false);
-        
-       
+        if(data.nextDialogueExist==false)
+        {
+            yield return new WaitUntil(() => moveToNextLine);
 
+            dialogueUI.SetActive(false);
+        }
+        else
+        {
+            //사전에서 인덱스 찾아서 그걸로 다시 대화
+        }
+
+    }
+
+    void ShowButtons(DialogueData data)
+    {
+        button1.gameObject.SetActive(true);
+        button2.gameObject.SetActive(true);
+        button1Text.SetText(data.choiceButtonText[0]);
+        button2Text.SetText(data.choiceButtonText[1]);
+       
     }
     
 }
