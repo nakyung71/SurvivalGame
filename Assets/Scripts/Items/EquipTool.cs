1e73a92cc60d9ae0f735d9498d250373ee72c1c3
kyubeom1 <a64778371@gmail.com>
2025-08-21 20:12:18 +0900

    11

--- Assets/Scripts/Items/EquipTool.cs
+++ Assets/Scripts/Items/EquipTool.cs
@@ -21,7 +21,7 @@ public class EquipTool : Equip
     {
         camera = Camera.main;
         animator = GetComponent<Animator>();
-        attackSound = GetComponent<AttackSound>();
+        attackSound = GetComponentInParent<AttackSound>();
     }
 
     public override void OnAttackInput()
