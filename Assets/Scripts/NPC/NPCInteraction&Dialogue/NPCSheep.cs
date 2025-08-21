101dcdb9a489f0bfcab68b8a543d886ee05e05db
nakyung71 <nakyung71@naver.com>
2025-08-21 23:51:34 +0900

    [feat] NPC 퀘스트 시스템

---
+++ Assets/Scripts/NPC/NPCInteraction&Dialogue/NPCSheep.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSheep : BaseNPC,ITalkable,IInteractable
+
+{
+    [SerializeField] DialogueData[] dialogueDatas;
+
+    public int TalkStep { get; private set; } = 0;
+
+    public string GetInteractPrompt()
+    {
+        return "장난꾸러기 같은 양이다";
+
+    }
+
+    public void OnInteract()
+    {
+        Talk();
+
+    }
+
+    public void Talk()
+    {
+        DialogueManager.Instance.SetTalk(dialogueDatas[0], this);
+
+    }
+
+
+}
